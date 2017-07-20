814cede19e198570d24501468a618346c7473255
emerson reyes <emersonreyes727@gmail.com>
2017-07-20 08:50:09 -0700

    Add move script

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour {
+	[SerializeField] private Transform target;
+
+	private NavMeshAgent nav;
+	private float speed = 2f;
+	private bool targetReached = false;
+
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!targetReached) {
+			nav.SetDestination (target.position);
+
+			if (transform.position.x == target.position.x) {
+				nav.enabled = false;
+				targetReached = true;	
+			}
+		} else {
+			if (transform.position.y >= 10f) {
+				transform.Translate (Vector3.right * (speed * Time.deltaTime));
+			} else {
+				transform.Translate (Vector3.up * (speed * Time.deltaTime));
+			}
+		}
+	}
+}
